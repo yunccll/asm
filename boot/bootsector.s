34c78fa2f2d3be340ab9c994a76f8ad7a546f750
yunccll <yunccll@sina.com>
2016-05-02 07:42:17 -0700

    add some int 0x10 call

--- boot/bootsector.s
+++ boot/bootsector.s
@@ -10,9 +10,36 @@ start2:
     movw %ax, %es
     movw %ax, %ss
     xorw %sp, %sp
+
+    # get the video mode 
+    movb $0x0f, %ah
+    int $0x10
+    movb %ah, nr_cols
+    movb %al, video_mode
+    movb %bh, cur_disp_page
+
+    # read the cursor position & size
+    movb $0x03, %ah
+    movb $0x00, %bh
+    int $0x10
+    # ch = 0x06   0b0000 0110 start scan line
+    # cl = 0x07   0b0000 0111 ending scan line
+    # dh = 0x12   row  (0-based)
+    # dl = 0x00   column
+
+    # set the cursor type
+    movb $0x01, %ah
+    movw $0x0607, %cx
+    int $0x10
+
+    #set the cursor position to 0,0
+    movb $0x02, %ah
+    movb $0x00, %bh
+    movw 0x0308, %dx  # dh -> row , dl -> columns
+    int $0x10
+
     sti
     cld
-
     movw  $boot_msg, %si
 
 msg_loop:
@@ -20,11 +47,20 @@ msg_loop:
     andb %al, %al
     jz reboot
     movb $0xe, %ah
-    movw $7,%bx
+    movw $1,%bx
     int $0x10
+    # colorful display , but no cursor advance
+    #movb $0x09, %ah
+    #movw $0x0001, %bx
+    #int $0x10
+    #mov $1, %cx
     jmp msg_loop
 
 reboot:
+    # get the video mode  again
+    movb $0x0f, %ah
+    int $0x10
+
     xorw %ax, %ax
     int $0x16
     int $0x19
@@ -35,3 +71,10 @@ boot_msg:
     .ascii "hello world from my os\r\n"
     .byte 0
 
+nr_cols:
+    .byte 0 # AH 0x50
+video_mode:
+    .byte 0 #al  0x03
+cur_disp_page:
+    .byte 0 #BH   00
+    
